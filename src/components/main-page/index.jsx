ee58c10dbbe198a50d90e79eb4c35e309ed03906
MrDuffs <chulkov.i@yandex.ru>
2023-07-30 16:02:01 +0300

    change class name

--- src/components/main-page/index.jsx
+++ src/components/main-page/index.jsx
@@ -1,13 +1,15 @@
 import { memo } from 'react';
 import LeftMenu from '../left-menu';
 import Profile from '../profile';
+import MainContent from '../main-content';
 import './style.scss';
 
 function MainPage() {
   return (
-    <div className="main-content">
+    <div className="main-page">
       <LeftMenu />
       <Profile />
+      <MainContent />
     </div>
   );
 }
