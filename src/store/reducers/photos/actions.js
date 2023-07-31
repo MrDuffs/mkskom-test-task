f333e567f816e0fa60ce8ed3269092a5a38e2d56
MrDuffs <chulkov.i@yandex.ru>
2023-07-31 20:14:10 +0300

    add search photos action and reducer cases

--- src/store/reducers/photos/actions.js
+++ src/store/reducers/photos/actions.js
@@ -6,36 +6,59 @@ export const GET_GRID_PHOTOS = 'GET_GRID_PHOTOS';
 export const GET_GRID_PHOTOS_SUCCESS = 'GET_GRID_PHOTOS_SUCCESS';
 export const GET_GRID_PHOTOS_ERROR = 'GET_GRID_PHOTOS_ERROR';
 
-export const getFlexboxPhotos = () => {
-  return async (dispatch) => {
-    dispatch({ type: 'GET_FLEXBOX_PHOTOS' });
+export const SEARCH_PHOTOS = 'SEARCH_PHOTOS';
+export const SEARCH_PHOTOS_SUCCESS = 'SEARCH_PHOTOS_SUCCESS';
+export const SEARCH_PHOTOS_ERROR = 'SEARCH_PHOTOS_ERROR';
 
-    try {
-      const response = await fetch('https://jsonplaceholder.typicode.com/photos');
-      const data = await response.json();
-      dispatch({
-        type: 'GET_FLEXBOX_PHOTOS_SUCCESS',
-        payload: data.slice(0, 16),
-      });
-    } catch (e) {
-      dispatch({ type: 'GET_FLEXBOX_PHOTOS_ERROR', payload: e });
-    }
-  };
+export const getFlexboxPhotos = () => async (dispatch) => {
+  dispatch({ type: 'GET_FLEXBOX_PHOTOS' });
+
+  try {
+    const response = await fetch('https://jsonplaceholder.typicode.com/photos');
+    const data = await response.json();
+    dispatch({
+      type: 'GET_FLEXBOX_PHOTOS_SUCCESS',
+      payload: data.slice(0, 16),
+    });
+  } catch (e) {
+    dispatch({ type: 'GET_FLEXBOX_PHOTOS_ERROR', payload: e });
+  }
 };
 
-export const getGridPhotos = () => {
-  return async (dispatch) => {
-    dispatch({ type: 'GET_GRID_PHOTOS' });
+export const getGridPhotos = () => async (dispatch) => {
+  dispatch({ type: 'GET_GRID_PHOTOS' });
 
-    try {
-      const response = await fetch('https://jsonplaceholder.typicode.com/photos');
-      const data = await response.json();
+  try {
+    const response = await fetch('https://jsonplaceholder.typicode.com/photos');
+    const data = await response.json();
+    dispatch({
+      type: 'GET_GRID_PHOTOS_SUCCESS',
+      payload: data.slice(0, 5),
+    });
+  } catch (e) {
+    dispatch({ type: 'GET_GRID_PHOTOS_ERROR', payload: e });
+  }
+};
+
+export const searchPhotos = (albumId, layout) => async (dispatch) => {
+  dispatch({ type: 'SEARCH_PHOTOS' });
+
+  try {
+    const response = await fetch(`https://jsonplaceholder.typicode.com/albums/${albumId}/photos`);
+    const data = await response.json();
+
+    if (layout === 'flex') {
       dispatch({
-        type: 'GET_GRID_PHOTOS_SUCCESS',
+        type: 'SEARCH_PHOTOS_SUCCESS',
+        payload: data.slice(0, 16),
+      });
+    } else {
+      dispatch({
+        type: 'SEARCH_PHOTOS_SUCCESS',
         payload: data.slice(0, 5),
       });
-    } catch (e) {
-      dispatch({ type: 'GET_GRID_PHOTOS_ERROR', payload: e });
     }
-  };
+  } catch (e) {
+    dispatch({ type: 'SEARCH_PHOTOS_ERROR', payload: e });
+  }
 };
--- src/store/reducers/photos/index.js
+++ src/store/reducers/photos/index.js
@@ -4,10 +4,14 @@ import {
   GET_FLEXBOX_PHOTOS_SUCCESS,
   GET_GRID_PHOTOS, GET_GRID_PHOTOS_ERROR,
   GET_GRID_PHOTOS_SUCCESS,
+  SEARCH_PHOTOS,
+  SEARCH_PHOTOS_SUCCESS,
+  SEARCH_PHOTOS_ERROR,
 } from './actions';
 
 const initialState = {
   data: [],
+  layout: 'flex',
   loading: false,
   error: null,
 };
@@ -18,7 +22,9 @@ export const photosReducer = (state = initialState, action) => {
     return { ...state, loading: true };
 
   case GET_FLEXBOX_PHOTOS_SUCCESS:
-    return { ...state, data: action.payload, loading: false };
+    return {
+      ...state, data: action.payload, layout: 'flex', loading: false,
+    };
 
   case GET_FLEXBOX_PHOTOS_ERROR:
     return { ...state, error: action.payload, loading: false };
@@ -27,9 +33,20 @@ export const photosReducer = (state = initialState, action) => {
     return { ...state, loading: true };
 
   case GET_GRID_PHOTOS_SUCCESS:
-    return { ...state, data: action.payload, loading: false };
+    return {
+      ...state, data: action.payload, layout: 'grid', loading: false,
+    };
 
   case GET_GRID_PHOTOS_ERROR:
+    return { ...state, error: action.payload, loading: false };
+
+  case SEARCH_PHOTOS:
+    return { ...state, loading: true };
+
+  case SEARCH_PHOTOS_SUCCESS:
+    return { ...state, data: action.payload, loading: false };
+
+  case SEARCH_PHOTOS_ERROR:
     return { ...state, error: action.payload, loading: false };
 
   default:
